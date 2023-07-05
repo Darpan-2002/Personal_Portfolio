2ebe6caa9605bebd01a27f71ad13a36be923f867
Darpan <darpandeka6000@gmail.com>
2023-07-05 21:28:49 +0530

    sticky navbar created

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ var typed = new Typed('#element', {
     strings: ['I am a devloper', '&amp; I am a programmer.'],
     typeSpeed: 60,
   });
+
+  window.onscroll = function() {myFunction()};
+
+  // Get the navbar
+  var navbar = document.getElementById("navbar");
+  
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+  
+  function myFunction() {
+    navbar.classList.add("sticky")
+  }
